8eda040f8cc6bf6ea76a74336af08b78392fbb14
weilong <weilong@indeed.com>
2018-01-30 16:29:50 -0800

    VWW-13: Remove unused test

--- src/test/java/com/indeed/vw/wrapper/learner/VWLearnersTest.java
+++ src/test/java/com/indeed/vw/wrapper/learner/VWLearnersTest.java
@@ -38,49 +38,5 @@ public class VWLearnersTest {
             learner.learn("1 |yo yo");
             learner.learn("2 |yo yo yay");
         }
-
-        print(tempFile);
-    }
-
-
-//    @Test
-//    public void testInvertHash() throws IOException {
-//        final File tempFile = File.createTempFile("file with whitespace", ".txt");
-//        tempFile.deleteOnExit();
-////        try (final VWFloatLearner learner = VowpalWabbit.builder().invertHash(tempFile.toPath())
-////                .adaptive().invariant().l2(0.0001).buildFloatLearner()) {
-////            learner.learn("1 |yo yo");
-////            learner.learn("2 |yo yo yay");
-////            learner.close();
-////        }
-//        try (
-//        final VWFloatLearner learner = VowpalWabbit.builder().initialRegressor(getFilePath("/VWUtilityTest/output")).invertHash(tempFile.toPath()).buildFloatLearner()){
-//
-//        }
-//
-//        print(tempFile);
-//    }
-
-    private Path getFilePath(@Nonnull final String relativePathToResources) throws IOException {
-        final Path modelDirectory = Files.createTempDirectory("test");
-        final Path modelPath = Paths.get(modelDirectory.toString(), "model");
-        try (final OutputStream os = Files.newOutputStream(modelPath);
-             final InputStream is = getClass().getResourceAsStream(relativePathToResources)
-        ) {
-            StreamUtils.copy(is, os);
-        }
-
-        return modelPath;
-    }
-
-    private void print(File tempFile) throws IOException {
-        try (final InputStream is = Files.newInputStream(tempFile.toPath())
-        ) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String currentline = null;
-            while((currentline = br.readLine()) != null) {
-                System.out.println(currentline);
-            }
-        }
     }
 }

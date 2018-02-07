ee82f9fbe52182ed04531eeed58c0f369cb97e04
weilong <weilong@indeed.com>
2018-02-07 15:39:45 -0800

    VW-13-2: Add osx lib file to the library, add unit test for 8.5.0 model and VWIntlearner

--- src/main/java/com/indeed/vw/wrapper/api/parameters/VWUtility.java
+++ src/main/java/com/indeed/vw/wrapper/api/parameters/VWUtility.java
@@ -1,11 +1,16 @@
 package com.indeed.vw.wrapper.api.parameters;
 
 import com.indeed.vw.wrapper.api.VowpalWabbit;
+import org.apache.log4j.lf5.util.StreamUtils;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Set of useful methods
@@ -32,5 +37,24 @@ public class VWUtility {
         } finally {
             Files.delete(readableModelPath);
         }
+    }
+
+    /**
+     *  Read vowpal wabbit model and count number of non-zero weights in it <p>
+     *
+     * @param relativePathToResources relative path to vowpal wabbit model
+     * @return the modelPath consumed by initialRegressor
+     * @throws IOException
+     */
+    public static Path getFilePathFromRelativePath(@Nonnull final String relativePathToResources) throws IOException {
+        final Path modelDirectory = Files.createTempDirectory("temp");
+        final Path modelPath = Paths.get(modelDirectory.toString(), "model");
+        try (final OutputStream os = Files.newOutputStream(modelPath);
+             final InputStream is = VWUtility.class.getResourceAsStream(relativePathToResources)
+        ) {
+            StreamUtils.copy(is, os);
+        }
+
+        return modelPath;
     }
 }
--- src/test/java/com/indeed/vw/wrapper/learner/VWLearnersTest.java
+++ src/test/java/com/indeed/vw/wrapper/learner/VWLearnersTest.java
@@ -1,8 +1,12 @@
 package com.indeed.vw.wrapper.learner;
 
 import com.indeed.vw.wrapper.api.VowpalWabbit;
+import com.indeed.vw.wrapper.api.example.ExampleBuilder;
+import com.indeed.vw.wrapper.api.parameters.VWUtility;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.lf5.util.StreamUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
@@ -37,6 +41,24 @@ public class VWLearnersTest {
                 .adaptive().invariant().l2(0.0001).buildFloatLearner()) {
             learner.learn("1 |yo yo");
             learner.learn("2 |yo yo yay");
+        }
+    }
+
+    @Test
+    public void testReadfromModelFile() throws IOException {
+        try(final VWFloatLearner learner = VowpalWabbit.builder()
+                .initialRegressor(VWUtility.getFilePathFromRelativePath("/VWLearnerTest/model.8.5.0")
+                ).buildFloatLearner()) {
+            Assert.assertEquals(0.6569876670837402, learner.predict("| price:.23 sqft:.25 age:.05 2006"), 0);
+        }
+    }
+
+    @Test
+    public void testIntLearner() throws IOException {
+        try(final VWIntLearner learner = VowpalWabbit.builder()
+                .initialRegressor(VWUtility.getFilePathFromRelativePath("/VWLearnerTest/classifier.model")
+                ).buildIntLearner()) {
+            Assert.assertEquals(2, learner.predict("| d e f"));
         }
     }
 }
